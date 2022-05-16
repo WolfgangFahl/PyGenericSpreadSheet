643b89c86732ec8aa7c0b079fc29d982b6fa36f5
Wolfgang Fahl <wf@bitplan.com>
2022-05-16 09:40:50 +0200

    fixes #9 regression

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,7 +8,7 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.0.16'
+    version='0.0.17'
     date = '2022-04-21'
     updated = '2022-05-16'
     name='pyGenericSpreadSheet'
