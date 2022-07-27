d584818e9cf75939e71f19d502f4f81108f1e849
Wolfgang Fahl <wf@bitplan.com>
2022-07-27 23:53:26 +0200

    triggers build

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,7 +8,7 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.0.22'
+    version='0.0.23'
     date = '2022-04-21'
     updated = '2022-07-27'
     name='pyGenericSpreadSheet'
