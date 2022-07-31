1a3b1b0203a031a7bfd8e3936ff08e88c85d38bd
Wolfgang Fahl <wf@bitplan.com>
2022-07-31 12:43:08 +0200

    prepares new release

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,7 +8,7 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.1.5'
+    version='0.1.6'
     date = '2022-04-21'
     updated = '2022-07-31'
     name='pyGenericSpreadSheet'
