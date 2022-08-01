ad3c19121d46749428ae2a104cd95bc299c72a90
Wolfgang Fahl <wf@bitplan.com>
2022-08-01 16:11:56 +0200

    upgrades

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,7 +8,7 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.1.8'
+    version='0.1.9'
     date = '2022-04-21'
     updated = '2022-08-01'
     name='pyGenericSpreadSheet'
