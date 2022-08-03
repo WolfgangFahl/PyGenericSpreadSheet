f8393c768af85ec86db03b68f520fade7ee261a7
Wolfgang Fahl <wf@bitplan.com>
2022-08-03 10:10:26 +0200

    upgrades

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,8 +8,8 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.1.12'
+    version='0.1.13'
     date = '2022-04-21'
-    updated = '2022-08-02'
+    updated = '2022-08-03'
     name='pyGenericSpreadSheet'
     description='python API providing generic Access to specific Spreadsheet backends'
