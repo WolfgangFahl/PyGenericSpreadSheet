fcbe0b97a71655e88710f602a5cb60ae6784ba8f
Wolfgang Fahl <wf@bitplan.com>
2022-08-07 17:43:29 +0200

    upgrades pyLodstorage

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,7 +8,7 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.1.17'
+    version='0.1.18'
     date = '2022-04-21'
     updated = '2022-08-07'
     name='pyGenericSpreadSheet'
