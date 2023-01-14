6f13f7a58dae1ac85e9adafef14707f14fc62427
tholzheim <tim.holzheim@rwth-aachen.de>
2023-01-14 01:50:18 +0100

    prepares v0.1.26

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,8 +8,8 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.1.25'
+    version='0.1.26'
     date = '2022-04-21'
-    updated = '2023-01-12'
+    updated = '2023-01-14'
     name='pyGenericSpreadSheet'
     description='python API providing generic Access to specific Spreadsheet backends'
