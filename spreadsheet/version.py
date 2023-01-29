3b7a4ce2d89589e55374e0120893d361288db06d
Wolfgang Fahl <wf@bitplan.com>
2023-01-29 21:05:41 +0100

    tries fixing dependencies

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,8 +8,8 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.1.29'
+    version='0.2.0'
     date = '2022-04-21'
-    updated = '2023-01-18'
+    updated = '2023-01-29'
     name='pyGenericSpreadSheet'
     description='python API providing generic Access to specific Spreadsheet backends'
