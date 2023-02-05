ef0cc68f6b5fdac9998fa8918b7dd89ccbfcdb8e
Wolfgang Fahl <wf@bitplan.com>
2023-02-05 11:04:14 +0100

    prepares release

--- spreadsheet/version.py
+++ spreadsheet/version.py
@@ -8,8 +8,8 @@ class Version(object):
     '''
     Version handling for pyGenericSpreadSheet
     '''
-    version='0.2.0'
+    version='0.2.1'
     date = '2022-04-21'
-    updated = '2023-01-29'
+    updated = '2023-02-05'
     name='pyGenericSpreadSheet'
     description='python API providing generic Access to specific Spreadsheet backends'
